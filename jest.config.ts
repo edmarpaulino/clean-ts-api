24bd69360287e6a0831f0e45e3168ad5f786248a
Edmar Paulino <edmarpaulino9@gmail.com>
2024-01-02 15:25:24 -0300

    chore: remove unnecessary files coverage in jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,11 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.d.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
