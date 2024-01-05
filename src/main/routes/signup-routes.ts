86973169dddc91738894a62439d2dc0901b9720f
Edmar Paulino <edmarpaulino9@gmail.com>
2024-01-05 13:00:45 -0300

    feat: add LoginValidationFactory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,7 +2,7 @@
 
 import type { Router } from 'express'
 import { adaptRoute } from '../adapter/express-route-adapter'
-import { makeSignUpController } from '../factories/signup'
+import { makeSignUpController } from '../factories/signup/signup'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
