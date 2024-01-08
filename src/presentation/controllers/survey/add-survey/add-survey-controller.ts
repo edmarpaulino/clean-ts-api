9ead1b6b53de5b67a8e7768121bfb79bdb394627
Edmar Paulino <edmarpaulino9@gmail.com>
2024-01-08 16:04:13 -0300

    feat: ensure AddSurveyController returns 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 import type { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -8,12 +8,16 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({ question, answers })
+      return { statusCode: 418, body: "I'm a teapot" }
+    } catch (error) {
+      return serverError(error as Error)
     }
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({ question, answers })
-    return { statusCode: 418, body: "I'm a teapot" }
   }
 }
