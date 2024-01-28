27db77fa6fce7ae95fc4ce8f64b1aaee9401b7f0
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-01-28 19:05:41 -0300

    feat: add route to serve static files

---
+++ src/main/config/static-files.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import express, { type Express } from 'express'
+import { resolve } from 'path'
+
+export default (app: Express): void => {
+  app.use('/static', express.static(resolve(__dirname, '../../static')))
+}
