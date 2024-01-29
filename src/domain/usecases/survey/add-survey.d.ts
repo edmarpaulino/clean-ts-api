667590089efd5f0d45a886ba66f79e224b75c9dc
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-01-29 11:56:37 -0300

    feat: change load surveys to add didAnswer field

--- src/domain/usecases/survey/add-survey.d.ts
+++ src/domain/usecases/survey/add-survey.d.ts
@@ -1,6 +1,6 @@
 import type { SurveyModel } from '@/domain/models/survey'
 
-export type AddSurveyParams = Omit<SurveyModel, 'id'>
+export type AddSurveyParams = Omit<SurveyModel, 'id' | 'didAnswer'>
 
 export interface AddSurvey {
   add: (surveyData: AddSurveyParams) => Promise<void>
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -7,7 +7,7 @@ import type {
   LoadSurveysRepository,
   SurveyModel
 } from '@/data/usecases/survey/load-surveys/db-load-surveys-protocols'
-import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { MongoHelper, QueryBuilder } from '@/infra/db/mongodb/helpers'
 
 export class SurveyMongoRepository
   implements
@@ -20,9 +20,42 @@ export class SurveyMongoRepository
     await surveyCollection.insertOne(data)
   }
 
-  async loadAll(): Promise<SurveyModel[]> {
+  async loadAll(accountId: string): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const surveys = await surveyCollection.find().toArray()
+    const query = new QueryBuilder()
+      .lookup({
+        from: 'surveyResults',
+        foreignField: 'surveyId',
+        localField: '_id',
+        as: 'result'
+      })
+      .project({
+        _id: 1,
+        question: 1,
+        answers: 1,
+        date: 1,
+        didAnswer: {
+          $gte: [
+            {
+              $size: {
+                $filter: {
+                  input: '$result',
+                  as: 'item',
+                  cond: {
+                    $eq: [
+                      '$$item.accountId',
+                      MongoHelper.generateObjectId(accountId)
+                    ]
+                  }
+                }
+              }
+            },
+            1
+          ]
+        }
+      })
+      .build()
+    const surveys = await surveyCollection.aggregate(query).toArray()
     return surveys.map((survey) => MongoHelper.map(survey))
   }
 
