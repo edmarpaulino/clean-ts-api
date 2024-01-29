b0284e901f311aa354930940c6c33a5882a40e7c
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-01-29 11:57:45 -0300

    docs: update survey result answer schema

--- src/main/docs/schemas/survey-result-answer-schema.ts
+++ src/main/docs/schemas/survey-result-answer-schema.ts
@@ -12,7 +12,10 @@ export const surveyResultAnswerSchema = {
     },
     percent: {
       type: 'number'
+    },
+    didAnswer: {
+      type: 'boolean'
     }
   },
-  required: ['answer', 'count', 'percent']
+  required: ['answer', 'count', 'percent', 'didAnswer']
 }
